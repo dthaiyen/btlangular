474f36a97e1b1b962280fc481a454611d6995ce9
dthaiyen <70678828+dthaiyen@users.noreply.github.com>
2020-10-12 13:39:32 +0700

    change

--- src/app/user/main/body/body.component.ts
+++ src/app/user/main/body/body.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Injector } from '@angular/core';
+import { BaseComponent } from '../../lib/base-component';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private injector: Injector, private http: HttpClient) { 
+    super(injector)
   }
-
+  itemnew: any;
+  ngOnInit(): void {
+    this._route.params.subscribe(params=>{
+      console.log("hello admin");
+      this.http.get("https://localhost:44334/api/Item/get_sanpham_new").subscribe(res=>{
+        this.itemnew = res;
+        setTimeout(()=>{
+          this.loadScripts();
+        })
+      })
+    })     
+  }
 }
