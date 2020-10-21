3a57bfd213f7144f7a3a7566d9dd474935b1ac40
dthaiyen <70678828+dthaiyen@users.noreply.github.com>
2020-10-21 08:18:41 +0700

    change

--- src/app/user/share/share.module.ts
+++ src/app/user/share/share.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [BannerComponent,FooterComponent,HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     BannerComponent,
