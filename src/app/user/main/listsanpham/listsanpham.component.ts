f784c12b568cbe3075cea6adbd5d56b72ea62da0
dthaiyen <70678828+dthaiyen@users.noreply.github.com>
2020-10-21 08:14:36 +0700

    change

--- src/app/user/main/listsanpham/listsanpham.component.ts
+++ src/app/user/main/listsanpham/listsanpham.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
 
+
+import { BaseComponent } from 'src/app/user/lib/base-component';
+import { Component, OnInit, Injector } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-listsanpham',
+  selector: 'app-single-product',
   templateUrl: './listsanpham.component.html',
   styleUrls: ['./listsanpham.component.css']
 })
-export class ListsanphamComponent implements OnInit {
-
-  constructor() { }
-
+export class SingleProductComponent extends BaseComponent implements OnInit {
+  
+  constructor(private _http:HttpClient, private _router: ActivatedRoute, private injector:Injector) {
+    super(injector)
+   }
+  ct : any;
+  ctlq:any;
   ngOnInit(): void {
+    this._router.params.subscribe(params=>{
+      let id = params["id"];
+      this._http.get("https://localhost:44374/api/sanpham/get-by-id/"+id).subscribe(res=>{
+        this.ct = res;
+        this._http.get("https://localhost:44374/api/sanpham/Get_Sanpham_lq/"+this.ct.maloai).subscribe(res=>{
+          this.ctlq = res;
+          setTimeout(()=>{
+            this.loadScripts();
+          })
+        });
+      });
+    })
   }
+  changequantiti(){
 
+  }
+  add_cart(ct){
+
+  }
 }
